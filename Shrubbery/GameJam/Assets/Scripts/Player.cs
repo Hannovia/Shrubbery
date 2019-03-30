06c58d19af0fd7bb4c2625fe398e5df37023ff35
Hannovia <33325512+Hannovia@users.noreply.github.com>
2019-03-30 18:50:22 +0100

    Shrubbery

---
+++ Shrubbery/GameJam/Assets/Scripts/Player.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private int cakeCount = 0;
+
+    [SerializeField] private float speed;
+
+    private Rigidbody2D rb;
+    private Vector2 moveVelocity;
+
+    private Animator anim;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput.normalized * speed;
+
+        if (moveInput.magnitude == 0)
+        {
+            anim.SetBool("isMoving", false);
+        }
+        else
+        {
+            anim.SetBool("isMoving", true);
+        }
+
+        if (cakeCount > 0 && Input.GetKeyDown(KeyCode.Space))
+        {
+            speedBoost();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    }
+
+    public void AddCake()
+    {
+        cakeCount++;
+    }
+    public int GetNumberOfCakes()
+    {
+        return cakeCount;
+    }
+
+    // if cakecount <0 and keydown x speed upp i x antal sekunder
+    private void speedBoost()
+    {
+        speed = 14f;
+    }
+}
+
+
---
+++ Shrubbery/GameJam/Assets/Scripts/PowerUp.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+       
+        
+        if (other.CompareTag("Player"))
+        {
+            Player player = other.GetComponent<Player>();
+            if(player == null)
+            {
+                Debug.Log("Other:  " + other);
+                Debug.Log("Player: " + player);
+            }
+            player.AddCake();
+            Pickup();
+        }
+    }
+
+    void Pickup()
+    {
+        Debug.Log("Power up picked up");
+        Destroy(this.gameObject);
+        
+    }
+}
