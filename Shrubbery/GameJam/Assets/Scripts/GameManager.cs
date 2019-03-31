6ce5c69998d105673512800faac4ebf30d28722c
Simon <simon.sundstrom95@gmail.com>
2019-03-31 13:43:26 +0200

    sdsd

--- Shrubbery/GameJam/Assets/Scripts/GameManager.cs
+++ Shrubbery/GameJam/Assets/Scripts/GameManager.cs
@@ -26,34 +26,40 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
-        scoreTexts[0].text = players[0].GetComponent<Player>().GetNumberOfCakes1().ToString();
-        scoreTexts[1].text = players[1].GetComponent<Player>().GetNumberOfCake2().ToString();
-        scoreTexts[2].text = players[2].GetComponent<Player>().GetNumberOfCake3().ToString();
-        scoreTexts[3].text = players[3].GetComponent<Player>().GetNumberOfCake4().ToString();
+        scoreTexts[0].text = players[0].GetComponent<Player>().GetNumberOfCakes().ToString();
+        scoreTexts[1].text = players[1].GetComponent<Player>().GetNumberOfCakes().ToString();
+        scoreTexts[2].text = players[2].GetComponent<Player>().GetNumberOfCakes().ToString();
+        scoreTexts[3].text = players[3].GetComponent<Player>().GetNumberOfCakes().ToString();
 
         if (timer.GetComponent<Timer>().timeLeft < 0)
         {
             GameOver();
         }
 
-     /*   Debug.Log(players[0].GetComponent<Player>().GetNumberOfCakes1());
-        Debug.Log(players[1].GetComponent<Player>().GetNumberOfCake2());
-        Debug.Log(players[2].GetComponent<Player>().GetNumberOfCake3());
-        Debug.Log(players[3].GetComponent<Player>().GetNumberOfCake4());
-*/
+        Debug.Log(players[0].GetComponent<Player>().GetNumberOfCakes());
+        Debug.Log(players[1].GetComponent<Player>().GetNumberOfCakes());
+        Debug.Log(players[2].GetComponent<Player>().GetNumberOfCakes());
+        Debug.Log(players[3].GetComponent<Player>().GetNumberOfCakes());
+
     }
 
     public void GameOver()
     {
-        
 
-        int[] cakes = {players[0].GetComponent<Player>().GetNumberOfCakes1(), players[1].GetComponent<Player>().GetNumberOfCake2(),
-        players[2].GetComponent<Player>().GetNumberOfCake3(), players[3].GetComponent<Player>().GetNumberOfCake3()};
+        GameObject currentWinner = null;
 
-        Array.Sort(cakes);
+        int mostCakes = -1;
 
-        Debug.Log("Player " + cakes[0] + " won!");
+        for(int currentPlayer = 0; currentPlayer < players.Length; currentPlayer++)
+        {
+            if(players[currentPlayer].GetComponent<Player>().GetNumberOfCakes() > mostCakes)
+            {
+                currentWinner = players[currentPlayer];
+                mostCakes = players[currentPlayer].GetComponent<Player>().GetNumberOfCakes();
+            }
+        }
 
+        Debug.Log("Winner: " + currentWinner.ToString());
         SceneManager.LoadScene("ScoresScene");
     }
 }
--- Shrubbery/GameJam/Assets/Scripts/PowerUp.cs
+++ Shrubbery/GameJam/Assets/Scripts/PowerUp.cs
@@ -6,6 +6,12 @@ public class PowerUp : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if (other.CompareTag("Player"))
+        {
+            Player player = other.GetComponent<Player>();
+            player.AddCake();
+            Pickup();
+        }
 
         if (other.CompareTag("Player1"))
         {
