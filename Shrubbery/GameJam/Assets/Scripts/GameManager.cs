cec3af9117d52434d497a0e83d005ef5e411a68c
Simon <simon.sundstrom95@gmail.com>
2019-03-31 12:26:28 +0200

    sd

--- Shrubbery/GameJam/Assets/Scripts/GameManager.cs
+++ Shrubbery/GameJam/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class GameManager : MonoBehaviour
     //Kanske ska vara gameobject istället för player
     [SerializeField] private GameObject[] players;
     [SerializeField] private GameObject[] spawnPoints;
+    [SerializeField] private GameObject[] scoreTexts;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,22 +29,25 @@ public class GameManager : MonoBehaviour
         {
             GameOver();
         }
+
+        
+        Debug.Log(players[0].GetComponent<Player>().GetNumberOfCakes1());
+        Debug.Log(players[1].GetComponent<Player>().GetNumberOfCake2());
+        Debug.Log(players[2].GetComponent<Player>().GetNumberOfCake3());
+        Debug.Log(players[0].GetComponent<Player>().GetNumberOfCake4());
+
     }
 
     public void GameOver()
     {
-        int mostCakes = -1;
-        int winner = -1;
-        for (int currentPlayer = 0; currentPlayer < players.Length; currentPlayer++)
-        {
-            if (players[currentPlayer].GetComponent<Player>().GetNumberOfCakes1() > mostCakes)
-            {
-                mostCakes = players[currentPlayer].GetComponent<Player>().GetNumberOfCakes1();
-                winner = currentPlayer;
-            }
-        }
+        
 
-        Debug.Log("Player " + winner + " won!");
+        int[] cakes = {players[0].GetComponent<Player>().GetNumberOfCakes1(), players[1].GetComponent<Player>().GetNumberOfCake2(),
+        players[2].GetComponent<Player>().GetNumberOfCake3(), players[3].GetComponent<Player>().GetNumberOfCake3()};
+
+        Array.Sort(cakes);
+
+        Debug.Log("Player " + cakes[0] + " won!");
 
     }
 }
